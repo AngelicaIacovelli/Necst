f4585c8ffab157b6ef6989d7bba7bee881e0361c
Guido Walter Di Donato <gw.didonato@gmail.com>
2021-09-15 17:52:01 +0200

    bug fixing

--- Angelica.cpp
+++ Angelica.cpp
@@ -1,55 +1,57 @@
 #include <iostream>
 #include <boost/math/distributions/poisson.hpp>
+#include <boost/graph/graph_traits.hpp>
 
 using namespace std;
 
 // Inizio lavorando su grafo non orientato
 
+template <typename MutableGraph, class RandNumGen>
+void generate_random_graph
+	(MutableGraph& g, 
+	typename boost::graph_traits<MutableGraph>::vertices_size_type num_nodes,
+	typename boost::graph_traits<MutableGraph>::vertices_size_type E,
+	RandNumGen& gen,
+	bool allow_parallel = true,
+	bool self_edges = false);
+
 int main()
 {
-    unsigned long num_nodes;
-    unsigned long density; 
-    
-    cout<<"Inserire numero di nodi: ";
-    cin>> num_nodes;
-    cout<<"Inserire il parametro di densita' in percentuale: ";
-    cin>> density;
+	unsigned long num_nodes;
+	unsigned long density; 
+	
+	cout<<"Inserire numero di nodi: ";
+	cin>> num_nodes;
+	cout<<"Inserire il parametro di densita' in percentuale: ";
+	cin>> density;
 
 
 /* GENERATE A RANDOM GRAPH
-    template <typename MutableGraph, class RandNumGen>
-  void generate_random_graph
-    (MutableGraph& g, 
-     typename graph_traits<MutableGraph>::vertices_size_type V,
-     typename graph_traits<MutableGraph>::vertices_size_type E,
-     RandNumGen& gen,
-     bool allow_parallel = true,
-     bool self_edges = false);
+	template <typename MutableGraph, class RandNumGen>
+	void generate_random_graph
+		(MutableGraph& g, 
+		typename graph_traits<MutableGraph>::vertices_size_type V,
+		typename graph_traits<MutableGraph>::vertices_size_type E,
+		RandNumGen& gen,
+		bool allow_parallel = true,
+		bool self_edges = false);
 
-     Effects: Adds V vertices and E edges, to g. Source and target vertices of each edge are randomly choosen. If self_edges is false, then no edge will have the same source and targets.
+Effects: Adds V vertices and E edges, to g. 
+Source and target vertices of each edge are randomly choosen. 
+If self_edges is false, then no edge will have the same source and targets.
 
 Precondition: num_vertices(g) == 0
 Compleixity: O(V*E)
 */
-   unsigned long L; // L è il numero di archi che voglio calcolare tramite la densità
-/* La densità di un grafo semplice (Δ) o non orientato è definita come: Δ = 2L / n(n-1)
-La densità di un grafo (Δ) orientato è definita come: Δ = L / n(n-1)                      */
-   L = num_nodes*(num_nodes-1)*density/2   //Caso non direzionato
-   unsigned int E; // Rendo intero il numero di archi
-   E = L;
-   
+	unsigned long L; // L è il numero di archi che voglio calcolare tramite la densità
 
-    template <typename MutableGraph, class RandNumGen>
-  void generate_random_graph
-    (MutableGraph& g, 
-     typename graph_traits<MutableGraph>::vertices_size_type num_nodes,
-     typename graph_traits<MutableGraph>::vertices_size_type E,
-     RandNumGen& gen,
-     bool allow_parallel = true,
-     bool self_edges = false);
+// La densità di un grafo semplice (Δ) o non orientato è definita come: Δ = 2L / n(n-1)
+// La densità di un grafo (Δ) orientato è definita come: Δ = L / n(n-1)                     
+	L = num_nodes*(num_nodes-1)*density/2;   //Caso non direzionato
+	unsigned int E; // Rendo intero il numero di archi
+	E = L;
 
+	return 0;
+}
 
-
-
-
-// NB: Ricordati di aggiungere preconditions righe:29
+// NB: Ricordati di aggiungere preconditions righe:42
