77185fc62cda5472018d101524b50904ec893706
Angelica Iacovelli <angelicaiacovelli99@libero.it>
2021-09-18 11:49:10 +0200

    updated Angelica.cpp

--- Angelica.cpp
+++ Angelica.cpp
@@ -2,6 +2,10 @@
 #include <boost/math/distributions/poisson.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include<cstdlib>
+#include <unistd.h>
+#include <ios>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -63,6 +67,50 @@ void dijkstra_shortest_paths_no_color_map
    VertexIndexMap index_map,
    DistanceCompare distance_compare, DistanceWeightCombine distance_weight_combine,
    DistanceInfinity distance_infinity, DistanceZero distance_zero);
+
+
+/* process_mem_usage(double &, double &) - takes two doubles by reference,
+ attempts to read the system-dependent data for a process' virtual memory
+ size and resident set size, and return the results in KB.
+
+ On failure, returns 0.0, 0.0 */
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+   using std::ios_base;
+   using std::ifstream;
+   using std::string;
+
+   vm_usage     = 0.0;
+   resident_set = 0.0;
+
+// 'file' stat seems to give the most reliable results
+   //
+   ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+   // dummy vars for leading entries in stat that we don't care about
+   //
+   string pid, comm, state, ppid, pgrp, session, tty_nr;
+   string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+   string utime, stime, cutime, cstime, priority, nice;
+   string O, itrealvalue, starttime;
+
+   // the two fields we want
+   //
+   unsigned long vsize;
+   long rss;
+
+   stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+               >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+               >> utime >> stime >> cutime >> cstime >> priority >> nice
+               >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+   stat_stream.close();
+
+   long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+   vm_usage     = vsize / 1024.0;
+   resident_set = rss * page_size_kb;
+// Sequenza funzioni memoria terminata
 
 
 int main()
@@ -127,6 +175,15 @@ auto stop = std::chrono::high_resolution_clock::now(); // record end time
 auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 std::cout << "Tempo impiegato: " << duration.count() << "\u00B5s" <<std::endl;
 
+// Misuro memoria utilizzata:
+// https://stackoverflow.com/questions/669438/how-to-get-memory-usage-at-runtime-using-c
+
+using std::cout;
+using std::endl;
+
+double vm, rss;
+process_mem_usage(vm, rss);
+cout << "VM: " << vm << "; RSS: " << rss << endl;
 
 	return 0;
 }
