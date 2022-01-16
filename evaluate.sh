71294d36295aedc7843c47ecfca11f9cebaa7f90
Guido Walter Di Donato <gw.didonato@gmail.com>
2022-01-16 16:00:25 +0100

    updated evaluation script to work with exeDef

--- evaluate.sh
+++ evaluate.sh
@@ -1,16 +1,16 @@
 printf "Starting shortest path evalutation\n"
-N_nodes=("1000" "2000" "4000" "8000" "16000" "32000")
+N_nodes=("1000" "2000" "4000" "8000" "16000" "32000" "64000" "128000")
 Density=('5' '10' '15' '20' '25' '30' '35' '40' '45' '50' '55' '60' '65' '70' '75' '80' '85' '90' '95' '100')
 Seeds=("10" "50" "100" "150" "200")
 ((i= ${#N_nodes[@]}*${#Density[@]}*${#Seeds[@]}))
 ((j=1))
 
-for nodes in "1000" "2000" "4000" "8000" "16000" "32000"; do
+for nodes in "1000" "2000" "4000" "8000" "16000" "32000" "64000" "128000"; do
     for density in '5' '10' '15' '20' '25' '30' '35' '40' '45' '50' '55' '60' '65' '70' '75' '80' '85' '90' '95' '100'; do
         for seeds in "10" "50" "100" "150" "200"; do
             printf "\nTest $j/$i\n"
             printf "\nNum_nodes = $nodes\nDensity = $density\nSeed = $seeds\n\n"
-            /usr/bin/time -lp ./exeDef $nodes $density test_data/r$seeds.fa &> results/N$nodes.D$density.R$seeds.log
+            ./exeDef $nodes $density $seeds &> results/N$nodes.D$density.S$seeds.log
             ((j+=1))
             printf "\nDone\n"
         done
