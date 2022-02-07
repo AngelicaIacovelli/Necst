fcbd65fed633f3bd311ca903a271c5b5e0b4c07a
angelica.iacovelli <angelicaiacovelli99@libero.it>
2022-02-07 09:08:37 +0100

    adding all the results

--- Def.cpp
+++ Def.cpp
@@ -49,7 +49,7 @@ argv[3] = seed
 */ 
 
     // flag per indicare se eseguire o meno Johnson
-    bool all_pairs = 1;
+    bool all_pairs = 0;
 
     // flag per indicare se stampare o meno i risultati di Dijstra (e Johnson)
     bool verbose = 0;
